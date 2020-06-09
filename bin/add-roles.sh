55e54b34e9985e54ec2d4e06fce15cd48308c115
Sam Walker <sam.walker@hmcts.net>
2020-06-09 10:41:50 +0100

    Update add-roles.sh

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -7,6 +7,7 @@ dir=$(dirname ${0})
 ${dir}/utils/idam-add-role.sh "ccd-import"
 ${dir}/utils/idam-add-role.sh "caseworker"
 ${dir}/utils/idam-add-role.sh "caseworker-cmc"
+${dir}/utils/ccd-add-role.sh "caseworker-cmc"
 
 # User used during the CCD import and ccd-role creation
 ${dir}/utils/idam-create-caseworker.sh "ccd.docker.default@hmcts.net" "ccd-import"
