ff803c932deac3727036d48e0257f8a33bbcd03e
Sam Walker <samw@quinn.mynet>
2020-06-25 14:04:49 +0100

    updated backend.yml, removed scheduler

--- bin/utils/create-scheduler-db.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker exec -it compose_ccd-shared-database_1 psql -U postgres -c "CREATE USER ucmc_scheduler WITH PASSWORD 'ucmc_scheduler'"
-docker exec -it compose_ccd-shared-database_1 psql -U postgres -c "CREATE DATABASE ucmc_scheduler WITH OWNER ucmc_scheduler"
