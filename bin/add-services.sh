5ac98fd257fc6e3290d73e3ae49bfb8c406a3375
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2020-07-17 09:57:59 +0100

    refactor: renamed ucmc to unspec (CMC-467) (#3)

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -6,6 +6,6 @@ dir=$(dirname ${0})
 
 ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect"
 
-${dir}/utils/idam-create-service.sh "ucmc" "ucmc" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
+${dir}/utils/idam-create-service.sh "unspec" "unspec" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
 
 ${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user"
