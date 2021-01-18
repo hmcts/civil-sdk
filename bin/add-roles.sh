c947599eea9fcf0b3697203bb094b3ebb28d52f7
Piotr Dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2021-01-18 11:27:36 +0000

    feat: added share a case roles (#26)

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -7,11 +7,15 @@ dir=$(dirname ${0})
 ${dir}/utils/idam-add-role.sh "ccd-import"
 ${dir}/utils/idam-add-role.sh "caseworker"
 ${dir}/utils/idam-add-role.sh "caseworker-civil"
+${dir}/utils/idam-add-role.sh "caseworker-caa"
+${dir}/utils/idam-add-role.sh "caseworker-approver"
 
 # User used during the CCD import and ccd-role creation
 ${dir}/utils/idam-create-caseworker.sh "ccd.docker.default@hmcts.net" "ccd-import"
 
 ${dir}/utils/ccd-add-role.sh "caseworker-civil"
+${dir}/utils/ccd-add-role.sh "caseworker-caa"
+${dir}/utils/ccd-add-role.sh "caseworker-approver"
 
 roles=("solicitor" "systemupdate")
 for role in "${roles[@]}"
