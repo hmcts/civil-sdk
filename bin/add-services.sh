f9ffe219d51cbee96e97e498cf92c40486f7b4fa
douglasrice <douglasrice@yahoo.com>
2021-10-08 11:23:40 +0100

    Use ccd ase document ami api (#11)

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -8,13 +8,13 @@ ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_sec
 
 ${dir}/utils/idam-create-service.sh "civil" "civil" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
 
-${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user"
+${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
 
-${dir}/utils/idam-create-service.sh "xui_mo_webapp" "xui_mo_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user manage-roles"
+${dir}/utils/idam-create-service.sh "xui_mo_webapp" "xui_mo_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user manage-roles search-user"
 
-${dir}/utils/idam-create-service.sh "xuiaowebapp" "xuiaowebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user"
+${dir}/utils/idam-create-service.sh "xuiaowebapp" "xuiaowebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
 
-${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid profile roles manage-user"
+${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid profile roles manage-user search-user"
 
 ${dir}/utils/idam-create-service.sh "rd_professional_api" "rd_professional_api" "OOOOOOOOOOOOOOOO" "https://rd_professional_api/oauth2redirect" "false" "openid profile roles create-user manage-user search-user"
 
@@ -22,4 +22,4 @@ ${dir}/utils/idam-create-service.sh "rd_user_profile_api" "rd_user_profile_api" 
 
 ${dir}/utils/idam-create-service.sh "am_role_assignment" "am_role_assignment" "am_role_assignment_secret" "http://localhost:4096/oauth2redirect" "false" "profile openid roles search-user"
 
-${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user"
+${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user search-user"
