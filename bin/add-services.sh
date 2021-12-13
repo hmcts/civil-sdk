c35165b08ac464209f69958fe75deb005cd24a42
smitatalawadekar-hmcts <smita.talawadekar@HMCTS.NET>
2021-12-13 13:08:09 +0000

    local change

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -14,7 +14,7 @@ ${dir}/utils/idam-create-service.sh "xui_mo_webapp" "xui_mo_webapp" "OOOOOOOOOOO
 
 ${dir}/utils/idam-create-service.sh "xuiaowebapp" "xuiaowebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
 
-${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid profile roles manage-user"
+${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid profile roles manage-user search-user"
 
 ${dir}/utils/idam-create-service.sh "rd_professional_api" "rd_professional_api" "OOOOOOOOOOOOOOOO" "https://rd_professional_api/oauth2redirect" "false" "openid profile roles create-user manage-user search-user"
 
@@ -22,4 +22,4 @@ ${dir}/utils/idam-create-service.sh "rd_user_profile_api" "rd_user_profile_api" 
 
 ${dir}/utils/idam-create-service.sh "am_role_assignment" "am_role_assignment" "am_role_assignment_secret" "http://localhost:4096/oauth2redirect" "false" "profile openid roles search-user"
 
-${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user"
+${dir}/utils/idam-create-service.sh "ccd_data_store_api" "ccd_data_store_api" "idam_data_store_client_secret" "http://ccd-data-store-api/oauth2redirect" "false" "profile openid roles manage-user search-user"
