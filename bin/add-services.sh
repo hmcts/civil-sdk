6d7f81bceddb367513789c7b0a5d92d52e2d52d2
Faran <94543556+faranhmcts@users.noreply.github.com>
2021-12-13 09:36:43 +0000

    ExUi Login Bug Fix (#16)

--- bin/add-services.sh
+++ bin/add-services.sh
@@ -8,11 +8,11 @@ ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_sec
 
 ${dir}/utils/idam-create-service.sh "civil" "civil" "OOOOOOOOOOOOOOOO" "https://localhost:9000/oauth2/callback"
 
-${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user"
+${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3333/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
 
-${dir}/utils/idam-create-service.sh "xui_mo_webapp" "xui_mo_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user manage-roles"
+${dir}/utils/idam-create-service.sh "xui_mo_webapp" "xui_mo_webapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user manage-roles search-user"
 
-${dir}/utils/idam-create-service.sh "xuiaowebapp" "xuiaowebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user"
+${dir}/utils/idam-create-service.sh "xuiaowebapp" "xuiaowebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3000/oauth2/callback" "false" "profile openid roles manage-user create-user search-user"
 
 ${dir}/utils/idam-create-service.sh "aac_manage_case_assignment" "aac_manage_case_assignment" "OOOOOOOOOOOOOOOO" "https://manage-case-assignment/oauth2redirect" "false" "openid profile roles manage-user"
 
