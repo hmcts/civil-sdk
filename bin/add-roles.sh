2312e3a54b9102cfd8c37bf6aacb2830c459c14b
drummondjm <93932689+drummondjm@users.noreply.github.com>
2022-08-10 11:23:43 +0100

    Feat/caseworker ras validation civ3585 (#54)
    * added caseworker-ras-validation
    
    * added caseworker-ras-validation, full-access and admin-access

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -24,7 +24,7 @@ do
   ${dir}/utils/ccd-add-role.sh "caseworker-civil-${role}"
 done
 
-accessprofiles=("judge-profile" "basic-access" "legal-adviser" "GS_profile")
+accessprofiles=("judge-profile" "basic-access" "legal-adviser" "GS_profile" "caseworker-ras-validation" "full-access" "admin-access")
 for accessprofile in "${accessprofiles[@]}"
 do
   ${dir}/utils/ccd-add-role.sh "${accessprofile}"
