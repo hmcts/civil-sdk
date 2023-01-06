5999405236972604f0b08d7d9ceaca9c83c8a793
= <gareth.lancaster1@hmcts.net>
2023-01-06 09:24:42 +0000

    Fixed orgs script

--- bin/wiremock/api/get_mappings.sh
+++ bin/wiremock/api/get_mappings.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 host="http://localhost:8765"
-getMappingsUrl="${host}/__admin/mappings/"
-mappingId=${1}
+getMappingsUrl="${host}/__admin/mappings"
 
 curl --insecure --fail --show-error --silent -X GET \
   "${getMappingsUrl}" \
