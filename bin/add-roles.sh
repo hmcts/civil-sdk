2fceb6d075eddfaeb1b9ee2fd1754c96250f0920
Gareth Lancaster <90632240+Gareth40343@users.noreply.github.com>
2023-05-05 11:11:53 +0100

    Added caseworker-wa-task-configuration back into add-roles script (#102)
    Co-authored-by: = <gareth.lancaster1@hmcts.net>

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -28,7 +28,7 @@ do
 done
 
 accessprofiles=("judge-profile" "basic-access" "ga-basic-access" "legal-adviser" "GS_profile" "caseworker-ras-validation" "full-access" "admin-access"
-"civil-administrator-basic" "civil-administrator-standard" "hearing-schedule-access" "APP-SOL-UNSPEC-PROFILE" "APP-SOL-SPEC-PROFILE" "RES-SOL-ONE-UNSPEC-PROFILE"
+"civil-administrator-basic" "civil-administrator-standard" "caseworker-wa-task-configuration" "hearing-schedule-access" "APP-SOL-UNSPEC-PROFILE" "APP-SOL-SPEC-PROFILE" "RES-SOL-ONE-UNSPEC-PROFILE"
 "RES-SOL-ONE-SPEC-PROFILE" "RES-SOL-TWO-UNSPEC-PROFILE" "RES-SOL-TWO-SPEC-PROFILE" "payment-access" "caseflags-admin" "caseflags-viewer" "hearing-manager" "hearing-viewer")
 
 for accessprofile in "${accessprofiles[@]}"
