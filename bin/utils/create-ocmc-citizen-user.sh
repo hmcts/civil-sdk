306809f658fd10bccde6f367eb4e5ababd92dc31
srochani <sunil.rochani@hotmail.com>
2024-03-22 10:04:51 +0000

    CIV-0000: OCMC local setup (#141)
    * CIV-0000: OCMC local setup
    
    * Update defaults.conf

---
+++ bin/utils/create-ocmc-citizen-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+USER_EMAIL="${1:-me@server.net}"
+ENV="${2:-local}"
+FORENAME="${3:-John}"
+SURNAME="${4:-Smith}"
+PASSWORD=Password12!
+USER_GROUP="citizens"
+USER_ROLES='[{"code":"citizen"}]'
+
+binFolder=$(dirname "$0")
+
+${binFolder}/create-user.sh "${USER_EMAIL}" "${ENV}" "${FORENAME}" "${SURNAME}" "${USER_ROLES}"
